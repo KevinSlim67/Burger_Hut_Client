5c052e865765ff582abb06a409b89ce2c008545b
KevinSlim67 <kevinslim167@gmail.com>
2022-11-19 00:03:00 +0200

    Navbar improvement

--- components/navbar.js
+++ components/navbar.js
@@ -6,13 +6,26 @@ class Navbar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <nav>
-            <img class="logo" src="./../assets/logo.png" alt="" />
-            <div>
+            <div class="brand">
+                <img class="logo" src="./../assets/logo.png" alt="" />
+                <h1>Burger Hut</h1>
+            </div>
+            <div class="links">
                 <a href="">About Us</a>
                 <a href="">Order</a>
                 <a href="">Contact Us</a>
             </div>
+            <div class="extra-links">
+                <button class="favorite" title="Favorites"></button>
+                <button class="order-history" title="Order History"></button>
+                <button class="address-book" title="Address Book"></button>
+                <button class="settings" title="Settings"></button>
+                <span>Username</span>
+                <button class="sign">Sign out</button>
+            </div>
+
             <button class="hamburger" onClick=""></button>
+
         </nav>
         `;
     }
