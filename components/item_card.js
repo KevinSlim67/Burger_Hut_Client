ffb87bb7cc57377f437308cdfa51545fb9987dde
KevinSlim67 <kevinslim167@gmail.com>
2022-12-27 22:28:39 +0200

    favorite system fully functional

--- components/item_card.js
+++ components/item_card.js
@@ -23,12 +23,14 @@ function fillItemsList(arr) {
 
 //create item card
 function createItem(item) {
-    const { id, name, price, image } = item;
+    const { id, name, price, image, category } = item;
     const itemCard = document.createElement('div');
     itemCard.classList.add('food-card');
-    checkFavorite(item, itemCard);
 
     itemCard.setAttribute('id', id);
+    itemCard.setAttribute('category', category);
+
+    checkFavorite(itemCard);
 
     itemCard.innerHTML =
         `
@@ -70,9 +72,10 @@ const getIngredients = async (id) => {
     }
 }
 
-function checkFavorite(element, itemCard) {
+function checkFavorite(itemCard) {
+    const id = itemCard.getAttribute('id');
     const userId = sessionStorage.getItem('userId');
-    fetch(`${url}/users-favorites/${userId}/${element.id}`, {
+    fetch(`${url}/users-favorites/${userId}/${id}`, {
         method: "GET",
         headers: {
             Accept: "application/json",
@@ -82,10 +85,8 @@ function checkFavorite(element, itemCard) {
         .then((res) => res.json())
         .then((res) => {
             const status = JSON.parse(res);
-            console.log(status);
-            element['isFavorite'] = status;
-            itemCard.setAttribute('favorite', element['isFavorite']);
-            if (element['isFavorite']) {
+            itemCard.setAttribute('favorite', status);
+            if (status) {
                 itemCard.classList.add('favorite');
             }
         })
@@ -98,17 +99,18 @@ function toggleFavorite(element) {
     const id = element.id.split('-')[0];
     const itemCard = document.getElementById(id);
     const isFavorite = JSON.parse(itemCard.getAttribute('favorite'));
-    if (element.isFavorite) {
-        removeFromFavorites(element);
+    if (isFavorite) {
+        removeFromFavorites(itemCard);
         itemCard.classList.remove('favorite');
         itemCard.setAttribute('favorite', false);
     } else {
+        addToFavorite(itemCard);
         itemCard.classList.add('favorite');
         itemCard.setAttribute('favorite', true);
     }
 }
 
-function removeFromFavorites(element) {
+function removeFromFavorites(itemCard) {
     fetch(`${url}/users-favorites/delete`, {
         method: "DELETE",
         headers: {
@@ -117,14 +119,14 @@ function removeFromFavorites(element) {
         },
         body: JSON.stringify({
             userId: sessionStorage.getItem('userId'),
-            foodId: element.id,
+            foodId: itemCard.id,
         })
     })
         .then((res) => res.json())
         .then((res) => {
             const url = window.location.href;
             if (url.includes('favorite')) getFavoriteItems(sessionStorage.getItem('userId'));
-            else getCategoryItems();
+            else getCategoryItems(itemCard.getAttribute('category'));
         })
         .catch((err) => {
             popup.setAttribute("status", "error");
@@ -133,102 +135,26 @@ function removeFromFavorites(element) {
         });
 }
 
-function showItemDetails(item) {
-    hideItemDetails(); //if another details pane is shown, delete it before displaying new one
-    const body = document.body;
-    const div = createItemDetails(item);
-    body.appendChild(div);
-}
-
-function incrementCounter() {
-    const counter = document.getElementsByClassName('number')[0];
-    let num = parseInt(counter.textContent);
-    if (num + 1 < 100) counter.textContent = ++num;
-}
-
-function decrementCounter() {
-    const counter = document.getElementsByClassName('number')[0];
-    let num = parseInt(counter.textContent);
-    if (num - 1 >= 0) counter.textContent = --num;
-}
-
-//removes food item details pane
-function hideItemDetails() {
-    const body = document.body;
-    const itemDetails = document.getElementsByClassName('food-details')[0];
-    if (itemDetails !== undefined) body.removeChild(itemDetails);
-}
-
-//add food item to cart, and specifies the amount added
-function addToCart(foodId) {
-    const amount = parseInt(document.getElementById('amount').textContent);
-
-    //if user isn't logged in, display a warning popup and exit out of function
-    if (sessionStorage.getItem('userId') === 'null') {
-        popup.setAttribute('status', 'error');
-        popup.setAttribute('text', `You need to login in order to add an item to your cart !`);
-        return false;
-    };
-
-    //if the amount set is 0, display a warning popup and exit out of function
-    if (amount === 0) {
-        popup.setAttribute('status', 'error');
-        popup.setAttribute('text', `You need to specify an amount !`);
-        return false;
-    };
-
-    //adds food item to cart
-    fetch(`${url}/foods/id/${foodId}`, {
-        method: "GET",
+function addToFavorite(itemCard) {
+    fetch(`${url}/users-favorites/add`, {
+        method: "POST",
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
         },
+        body: JSON.stringify({
+            userId: sessionStorage.getItem('userId'),
+            foodId: itemCard.id,
+        })
     })
         .then((res) => res.json())
-        .then((res) => {
-            //if item already present in cart, just increment amount instead of adding the item again
-            const cart = JSON.parse(sessionStorage.getItem('cart'));
-
-            //checks if item is already in cart, if it is, increment its amount instead of adding it again
-            let exists = false;
-            cart.forEach(i => {
-                if (i.id === foodId) {
-                    i.amount = i.amount + amount;
-                    exists = true;
-                }
-            });
-            if (!exists) cart.push({ ...res, amount: amount });
-
-            sessionStorage.setItem('cart', JSON.stringify(cart));
-            updateCartBtnAmount();
-            handleSuccessPopup(amount, res);
-        })
         .catch((err) => {
-            popup.setAttribute('status', 'error');
-            popup.setAttribute('text', `Uh oh, there appears to have been a problem. If the issue persists, try again later.`);
-            console.error(err)
+            popup.setAttribute("status", "error");
+            popup.setAttribute("text", `Uh oh, we couldn't add the item from your favorites.`);
+            console.error(err);
         });
 }
 
-//handles the success popup that appear if addToCart() is successful
-function handleSuccessPopup(amount, res) {
-    popup.setAttribute('status', 'success');
-    popup.setAttribute('text', `${amount + temporaryTotalAmount} ${res.name}(s) added to cart`);
-    //if the timer of the first timer isn't done, then cancel it, and start a new timer
-    //with the amount updated
-    if (timerId === null) {
-        timerId = setTimeout(() => {
-            temporaryTotalAmount = 0;
-        }, 5000);
-    } else {
-        clearTimeout(timerId);
-        timerId = setTimeout(() => {
-            temporaryTotalAmount = 0;
-        }, 5000);
-    }
-    temporaryTotalAmount += amount;
-}
 
 //updates the cart button amount that is on the bottom right of the screen
 function updateCartBtnAmount() {
@@ -236,57 +162,4 @@ function updateCartBtnAmount() {
     const total = cart.reduce((total, item) => total + item.amount, 0);
     const btn = document.getElementById('cart-btn-amount');
     btn.innerText = total;
-}
-
-//displays a box that displays all details about item such as ingredients and cooktime 
-function createItemDetails(item) {
-    const { id, name, price, image, rating, cooktime, ingredients } = item;
-
-    const ratingDiv = (rating !== null) ? `<h5 class="rating">${rating}</h5>` : '';
-    const cookTimeDiv = (cooktime !== null) ? `<h5 class="cook-time">${cooktime} min</h5>` : '';
-
-    const ingredientDiv = (ingredients.length > 0) ? ` 
-    <div>
-        <h5>Ingredients</h5>
-        <p>${ingredients.join(' - ')}<p/>
-    </div>` : '';
-
-    const itemDetails = document.createElement('div');
-    itemDetails.classList.add('food-details');
-    itemDetails.innerHTML = `
-    <img src="data:image/png;base64, ${image}">
-    <div class="details">
-        <div class="info">
-            <div>
-                <div class="split-container">
-                    <div class="left">
-                        <h3>${name}</h3>
-                        ${ratingDiv}
-                        ${cookTimeDiv}
-                    </div>
-                    <div class="right"> 
-                        <h4>$${price}</h4>
-                        <div class="counter">
-                        <button onclick="decrementCounter(this)">-</button>
-                        <span id="amount" class="number">0</span>
-                        <button onclick="incrementCounter(this)">+</button>
-                        </div>
-                    </div>
-                </div>
-                <hr>
-                ${ingredientDiv}
-            </div>
-            <button class="type1 full cart" onclick="addToCart(${id})">
-                <img src="./../assets/icons/cart.png" alt="" />
-                Add To Cart
-            </button>
-        </div>
-        <div class="image">
-            <img src="data:image/png;base64, ${image}" alt="${name}">
-        </div>
-    </div>
-    <button class="back" onclick="hideItemDetails()"></button>
-    <button class="favorite"></button>
-    `;
-    return itemDetails;
 }
---
+++ components/item_card_details.js
@@ -0,0 +1,149 @@
+function showItemDetails(item) {
+    hideItemDetails(); //if another details pane is shown, delete it before displaying new one
+    const body = document.body;
+    const div = createItemDetails(item);
+    body.appendChild(div);
+}
+
+function incrementCounter() {
+    const counter = document.getElementsByClassName('number')[0];
+    let num = parseInt(counter.textContent);
+    if (num + 1 < 100) counter.textContent = ++num;
+}
+
+function decrementCounter() {
+    const counter = document.getElementsByClassName('number')[0];
+    let num = parseInt(counter.textContent);
+    if (num - 1 >= 0) counter.textContent = --num;
+}
+
+//removes food item details pane
+function hideItemDetails() {
+    const body = document.body;
+    const itemDetails = document.getElementsByClassName('food-details')[0];
+    if (itemDetails !== undefined) body.removeChild(itemDetails);
+}
+
+//add food item to cart, and specifies the amount added
+function addToCart(foodId) {
+    const amount = parseInt(document.getElementById('amount').textContent);
+
+    //if user isn't logged in, display a warning popup and exit out of function
+    if (sessionStorage.getItem('userId') === 'null') {
+        popup.setAttribute('status', 'error');
+        popup.setAttribute('text', `You need to login in order to add an item to your cart !`);
+        return false;
+    };
+
+    //if the amount set is 0, display a warning popup and exit out of function
+    if (amount === 0) {
+        popup.setAttribute('status', 'error');
+        popup.setAttribute('text', `You need to specify an amount !`);
+        return false;
+    };
+
+    //adds food item to cart
+    fetch(`${url}/foods/id/${foodId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            //if item already present in cart, just increment amount instead of adding the item again
+            const cart = JSON.parse(sessionStorage.getItem('cart'));
+
+            //checks if item is already in cart, if it is, increment its amount instead of adding it again
+            let exists = false;
+            cart.forEach(i => {
+                if (i.id === foodId) {
+                    i.amount = i.amount + amount;
+                    exists = true;
+                }
+            });
+            if (!exists) cart.push({ ...res, amount: amount });
+
+            sessionStorage.setItem('cart', JSON.stringify(cart));
+            updateCartBtnAmount();
+            handleSuccessPopup(amount, res);
+        })
+        .catch((err) => {
+            popup.setAttribute('status', 'error');
+            popup.setAttribute('text', `Uh oh, there appears to have been a problem. If the issue persists, try again later.`);
+            console.error(err)
+        });
+}
+
+//handles the success popup that appear if addToCart() is successful
+function handleSuccessPopup(amount, res) {
+    popup.setAttribute('status', 'success');
+    popup.setAttribute('text', `${amount + temporaryTotalAmount} ${res.name}(s) added to cart`);
+    //if the timer of the first timer isn't done, then cancel it, and start a new timer
+    //with the amount updated
+    if (timerId === null) {
+        timerId = setTimeout(() => {
+            temporaryTotalAmount = 0;
+        }, 5000);
+    } else {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            temporaryTotalAmount = 0;
+        }, 5000);
+    }
+    temporaryTotalAmount += amount;
+}
+
+//displays a box that displays all details about item such as ingredients and cooktime 
+function createItemDetails(item) {
+    const { id, name, price, image, rating, cooktime, ingredients } = item;
+
+    const ratingDiv = (rating !== null) ? `<h5 class="rating">${rating}</h5>` : '';
+    const cookTimeDiv = (cooktime !== null) ? `<h5 class="cook-time">${cooktime} min</h5>` : '';
+
+    const ingredientDiv = (ingredients.length > 0) ? ` 
+    <div>
+        <h5>Ingredients</h5>
+        <p>${ingredients.join(' - ')}<p/>
+    </div>` : '';
+
+    const itemDetails = document.createElement('div');
+    itemDetails.classList.add('food-details');
+    itemDetails.innerHTML = `
+    <img src="data:image/png;base64, ${image}">
+    <div class="details">
+        <div class="info">
+            <div>
+                <div class="split-container">
+                    <div class="left">
+                        <h3>${name}</h3>
+                        ${ratingDiv}
+                        ${cookTimeDiv}
+                    </div>
+                    <div class="right"> 
+                        <h4>$${price}</h4>
+                        <div class="counter">
+                        <button onclick="decrementCounter(this)">-</button>
+                        <span id="amount" class="number">0</span>
+                        <button onclick="incrementCounter(this)">+</button>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                ${ingredientDiv}
+            </div>
+            <button class="type1 full cart" onclick="addToCart(${id})">
+                <img src="./../assets/icons/cart.png" alt="" />
+                Add To Cart
+            </button>
+        </div>
+        <div class="image">
+            <img src="data:image/png;base64, ${image}" alt="${name}">
+        </div>
+    </div>
+    <button class="back" onclick="hideItemDetails()"></button>
+    <button class="favorite"></button>
+    `;
+    return itemDetails;
+}
